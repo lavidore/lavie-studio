c7424e8d9e88a237f594b15430111caedda80864
Laviele <148462796+lavidore@users.noreply.github.com>
2023-12-04 14:57:28 +0700

    Update tulam.js

--- js/tulam.js
+++ js/tulam.js
@@ -28,6 +28,7 @@
       }
     })
   }
+
   /*to top button*/
   // Get the button:
   let mybutton = document.getElementById("myBtn");
@@ -119,9 +120,49 @@ for(let i = 0; i < addButtons.length; i++) {
     cart.innerText = parseInt(item, 10) + 1;
   }
 }
-
 // clear cart 
 document.querySelector('.clear-cart').addEventListener('click', function() {
   document.getElementById('cart').textContent = '0';
 });
 
+// hover img
+$(".image-swap").hover( 
+  function () { $(this).addClass("hovered"); } ,
+  function () { $(this).removeClass("hovered"); }
+);
+
+// slideshow automatic and manual
+var slideIndex = 1;
+var timer = null;
+      showSlides(slideIndex);
+      function plusSlides(n) {
+        clearTimeout(timer);
+        showSlides(slideIndex += n);
+      }
+      function currentSlide(n) {
+        clearTimeout(timer);
+        showSlides(slideIndex = n);
+      }
+      function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if(n==undefined){
+          n= ++slideIndex
+        }
+        if(n > slides.length) {
+          slideIndex = 1
+        }
+        if(n < 1) {
+          slideIndex = slides.length
+        }
+        for(i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        for(i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        timer=setTimeout(showSlides,5500);
+      }
